386c32252e5c0ade5d9b22a5ac095043a45c85b7
alex <dukhnovskiy.a@n-t.io>
2020-10-30 13:07:52 +0300

    Add Hello World app

---
+++ 1-hello-world/dist/app.controller.d.ts
@@ -0,0 +1,12 @@
+export declare class AppController {
+    private helloWorldText;
+    private helloWorldState;
+    get(): string;
+    getName(name: string): string;
+    getAdvNameHelloWorld(name: string): string;
+    postHelloWorld(name: string): string;
+    putStateHelloWorld(name: string): string;
+    deleteStateHelloWorld(): string;
+    getStateHelloWorld(): string;
+    private getHelloText;
+}
---
+++ 1-hello-world/src/app.controller.ts
@@ -0,0 +1,65 @@
+import {
+  Controller,
+  Get,
+  Query,
+  Param,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
+
+@Controller('app')
+export class AppController {
+  private helloWorldText = 'Hello World!';
+  private helloWorldState: string;
+
+  // Simple Get
+  @Get('simple')
+  public get(): string {
+    return this.helloWorldText;
+  }
+
+  // Simple Get with param
+  @Get('simple/:name')
+  public getName(@Param('name') name: string): string {
+    return this.getHelloText(name);
+  }
+
+  // Advanced Get with param
+  @Get('/adv')
+  public getAdvNameHelloWorld(@Query('name') name: string): string {
+    console.log(name);
+    return this.getHelloText(name);
+  }
+
+  // Simple Post
+  @Post('simple')
+  public postHelloWorld(@Body('name') name: string): string {
+    return this.getHelloText(name);
+  }
+
+  // Simple Put
+  @Put('simple/:name')
+  public putStateHelloWorld(@Param('name') name: string): string {
+    this.helloWorldState = name;
+
+    return `Hello World state was changed to ${this.helloWorldState}`;
+  }
+
+  // Simple Delete
+  @Delete('simple')
+  public deleteStateHelloWorld(): string {
+    return `Hello World state was deleted`;
+  }
+
+  // Get Hello World state
+  @Get('state')
+  public getStateHelloWorld(): string {
+    return this.getHelloText(this.helloWorldState);
+  }
+
+  private getHelloText(text: string): string {
+    return `Hello ${text}!`;
+  }
+}
