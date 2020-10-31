93c8d7922b845afcf2cc3c01a1fedd4be60a8d56
alex <dukhnovskiy.a@n-t.io>
2020-10-31 11:25:24 +0300

    Remove trash

--- 02-link-converter/src/custom-exception.filter.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CustomExceptionFilter } from './custom-exception.filter';
-
-describe('CustomExceptionFilter', () => {
-  it('should be defined', () => {
-    expect(new CustomExceptionFilter()).toBeDefined();
-  });
-});
